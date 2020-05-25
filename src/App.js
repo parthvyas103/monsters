e37f1396e228ced413eac8a13074d3944528fae0
Parth <parthvyas103@gmail.com>
2020-05-25 17:12:42 +0530

    Addig the files

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component{
       );
     return (
     <div className="App">
+      <h1>Monsters Rolodex</h1>
       <SearchBox 
       placeHolder = "search monsters"
       handleChange = {this.handleChange} />
