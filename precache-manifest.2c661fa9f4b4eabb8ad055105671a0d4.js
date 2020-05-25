00ee32dc33057b972403bbfa981a346877180973
Parth <parthvyas103@gmail.com>
2020-05-25 17:09:33 +0530

    Updates

---
+++ precache-manifest.2c661fa9f4b4eabb8ad055105671a0d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ec66833bf78247c0b485533e3a896c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "41ccca2e4062c963aba1",
+    "url": "/monsters/static/css/main.ac661ff8.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/monsters/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ccca2e4062c963aba1",
+    "url": "/monsters/static/js/main.34698a5e.chunk.js"
+  },
+  {
+    "revision": "5520c3e6235208f1eea1",
+    "url": "/monsters/static/js/runtime-main.59ed3f86.js"
+  }
+]);
